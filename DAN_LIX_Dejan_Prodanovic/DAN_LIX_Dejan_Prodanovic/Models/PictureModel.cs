af6bdff247a8e3c37324c009b7e3d1472991f3cb
dexpro988 <dejan.prodanovic@panonit.com>
2020-09-02 06:33:06 -0700

    Project added

---
+++ DAN_LIX_Dejan_Prodanovic/DAN_LIX_Dejan_Prodanovic/Models/PictureModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAN_LIX_Dejan_Prodanovic.Models
+{
+    public class PictureModel
+    {
+        public int Id { get; set; }
+        public string ImageSource { get; set; }
+    }
+}
