8025c728768477adf6240da8d2f661f8f2ba03be
dexpro988 <dejan.prodanovic@panonit.com>
2020-09-02 06:38:27 -0700

    MainWindow created

---
+++ DAN_LIX_Dejan_Prodanovic/DAN_LIX_Dejan_Prodanovic/Views/MainMenuView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DAN_LIX_Dejan_Prodanovic.Views
+{
+    /// <summary>
+    /// Interaction logic for MainMenuView.xaml
+    /// </summary>
+    public partial class MainMenuView : UserControl
+    {
+        public MainMenuView()
+        {
+            InitializeComponent();
+        }
+
+        private void Slide_Clicked(object sender, RoutedEventArgs e)
+        {
+            var game = DataContext as GameViewModel;
+            var button = sender as Button;
+            game.ClickedSlide(button.DataContext);
+        }
+
+        private void PlayAgain_C(object sender, RoutedEventArgs e)
+        {
+            var game = DataContext as GameViewModel;
+            game.Restart();
+        }
+    }
+}
